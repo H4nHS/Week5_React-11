8f65ed5e84e78fba1fdf22337e8e6709416752ed
Jongco <narira0922@gmail.com>
2022-08-05 17:42:38 +0900

    Store, slice, form Component

---
+++ src/app/slice/CreateSlice.jsx
@@ -0,0 +1,40 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+
+const initialState = [
+    {
+        title: '초기값 입니다',
+        content: '초기값 입니다',
+        id: 0
+    }
+]
+
+export const storeSlice = createSlice({
+    name: 'storeSlice',
+    initialState,
+    reducers: {
+        // : 추가
+        add: (state, action) => {
+            state.push({
+                title: action.payload.title,
+                content: action.payload.content,
+                id: action.payload.id
+            })
+        },
+        // payload로 id 전달해야함 : 삭제
+        del: (state, action) => {
+            return current(state).filter((value) => value.id !== action.payload)
+        },
+        // id, title, content 전달 : 수정 
+        rev: (state, action) => {
+            return current(state).map((value) => {
+                if (value.id === action.payload.id) {
+                    return value = { ...value, title: action.payload.title, content: action.payload.content }
+                } else {
+                    return value
+                }
+            })
+        }
+    }
+})
+
+export const { add, rev, del } = storeSlice.actions 
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { storeSlice } from "./slice/CreateSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    store: storeSlice.reducer,
   },
 });
